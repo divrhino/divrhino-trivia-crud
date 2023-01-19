181b5cf43cd9b4c63b852c763b7ce591716cb360
Div Rhino <fancydresspenguin+divrhino@gmail.com>
2023-01-19 20:28:41 +0800

    Add `edit` and `update` routes

--- cmd/routes.go
+++ cmd/routes.go
@@ -12,4 +12,7 @@ func setupRoutes(app *fiber.App) {
 	app.Post("/fact", handlers.CreateFact)
 
 	app.Get("/fact/:id", handlers.ShowFact)
+
+	app.Get("/fact/:id/edit", handlers.EditFact)
+	app.Patch("/fact/:id", handlers.UpdateFact)
 }
