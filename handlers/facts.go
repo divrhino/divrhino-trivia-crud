583a7fe75d8f91f37bcf6feb5be92711ae6b8c78
Div Rhino <fancydresspenguin+divrhino@gmail.com>
2023-01-19 20:29:24 +0800

    Add `edit` handler

--- handlers/facts.go
+++ handlers/facts.go
@@ -53,6 +53,22 @@ func ShowFact(c *fiber.Ctx) error {
 	})
 }
 
+func EditFact(c *fiber.Ctx) error {
+	fact := models.Fact{}
+	id := c.Params("id")
+
+	result := database.DB.Db.Where("id = ?", id).First(&fact)
+	if result.Error != nil {
+		return NotFound(c)
+	}
+
+	return c.Render("edit", fiber.Map{
+		"Title":    "Edit Fact",
+		"Subtitle": "Edit your interesting fact",
+		"Fact":     fact,
+	})
+}
+
 func NotFound(c *fiber.Ctx) error {
 	return c.Status(fiber.StatusNotFound).SendFile("./public/404.html")
 }
