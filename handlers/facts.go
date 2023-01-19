a2135b8ff3c66aeb309342ec2d57ebe5f33941a5
Div Rhino Dev <65655808+divrhino@users.noreply.github.com>
2023-01-19 20:43:27 +0800

    Step 4 - Update action (#5)
    * Add `edit` and `update` routes
    * Add `edit` handler
    * Add `update` handler
    * Add `edit` form
    * Add existing values to `edit` form fields
    * Move js script import into `main` layout
    * Add `id` attributes to stylesheet and js imports
    * Add workaround to bust cache. Development only
    * Add `PATCH` request with `fetch` API

--- handlers/facts.go
+++ handlers/facts.go
@@ -53,6 +53,38 @@ func ShowFact(c *fiber.Ctx) error {
 	})
 }
 
+func EditFact(c *fiber.Ctx) error {
+	fact := models.Fact{}
+	id := c.Params("id")
+
+	result := database.DB.Db.Where("id = ?", id).First(&fact)
+	if result.Error != nil {
+		return NotFound(c)
+	}
+
+	return c.Render("edit", fiber.Map{
+		"Title":    "Edit Fact",
+		"Subtitle": "Edit your interesting fact",
+		"Fact":     fact,
+	})
+}
+
+func UpdateFact(c *fiber.Ctx) error {
+	fact := new(models.Fact)
+	id := c.Params("id")
+
+	if err := c.BodyParser(fact); err != nil {
+		return c.Status(fiber.StatusServiceUnavailable).SendString(err.Error())
+	}
+
+	result := database.DB.Db.Model(&fact).Where("id = ?", id).Updates(fact)
+	if result.Error != nil {
+		return EditFact(c)
+	}
+
+	return ShowFact(c)
+}
+
 func NotFound(c *fiber.Ctx) error {
 	return c.Status(fiber.StatusNotFound).SendFile("./public/404.html")
 }
